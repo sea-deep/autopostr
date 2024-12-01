a7306756092dcfae753a4d283714aa97a58921b2
sea-deep <seadeepie@gmail.com>
2024-12-01 20:54:25 +0530

    Auto Update
    M  public/dashboard.html
    M  routes/auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -84,11 +84,5 @@ router.get('/status', (req, res) => {
     }
 });
 
-router.get('/logout', (req, res, next) => {
-    req.logout((err) => {
-        if (err) { return next(err); }
-        res.redirect('/');
-    });
-});
 
 module.exports = router;
